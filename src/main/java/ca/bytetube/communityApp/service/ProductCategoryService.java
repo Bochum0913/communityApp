b60470e9f20dfa6f4f14a6cbcee0c1fd596c1b4c
Bochum0913 <lbh19920913@gmail.com>
2024-10-02 23:08:26 -0400

    added product Category List

---
+++ src/main/java/ca/bytetube/communityApp/service/ProductCategoryService.java
@@ -0,0 +1,17 @@
+package ca.bytetube.communityApp.service;
+
+
+import ca.bytetube.communityApp.entity.ProductCategory;
+
+
+import java.util.List;
+
+public interface ProductCategoryService {
+	/**
+	 * 查询指定某个店铺下的所有商品类别信息
+	 */
+	List<ProductCategory> getProductCategoryList(long shopId);
+
+
+
+}
